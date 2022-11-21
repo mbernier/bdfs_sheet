2bebdd1cc2889ea7d3a3a7eb297683eab4d3c4a3
Matt Bernier <mkbernier@gmail.com>
2022-11-21 15:47:08 -0700

    Removed the google python lib items
    since I'm using gspread, i don't need the google lib items

--- modules/sheet/bdfs_sheet.py
+++ modules/sheet/bdfs_sheet.py
@@ -1,16 +1,10 @@
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
 import gspread
 
 class Bdfs_sheet:
     def _init_():
         self.data = []
 
-    def read_sheet(this, creds):
+    def read_sheet(this):
         # The ID and range of a sample spreadsheet.
         SPREADSHEET_ID = '1RyODmeydoIlMO75xa5wIxHRxqnZWRkDcxWZyp6fK-H8'
         SAMPLE_RANGE_NAME = 'sarto_barn_single!D2:CR'
